cc6d5a2a7ef5b2183dac981d6e3d316bb111f636
Ekta Chaudhary <60789158+itsekta@users.noreply.github.com>
2022-11-18 12:50:16 +0530

    Added Comparison Section

---
+++ components/Comparison.vue
@@ -0,0 +1,201 @@
+<template>
+  <div>
+    <div class="row pinkBg miniatureIntro" id="AboutUs">
+      <div class="col">
+        <h3>
+          Introducing the Most High<br />
+          Resolution Miniature in the World
+        </h3>
+      </div>
+    </div>
+    <div class="row pinkBg blurryImg">
+      <div class="col-1"></div>
+      <div class="col-md-4 col-sm-12 ShowSecond">
+        <div class="paraRight compareSection">
+          <h4>not a blurry face</h4>
+          <span>High resolution coloring job</span>
+          <p>
+            The color resolution of other brands is very low that’s why it’s
+            looks blur and detailing of facial features can’t be seen, eyes are
+            the most important part of a person so it shouldn’t look like
+            garbage but on the other hand DittoDolls deliver high resolution
+            facial features with attractive eyes.
+          </p>
+          <a
+            href="https://wa.me/+918700101645?text=hello I'm interested in buying your miniatures kindly guide me the order instructions…"
+            class="btn cutomizeBtn"
+            target="_blank"
+            >Customize Now <img src="../assets/images/arrow-right.svg"
+          /></a>
+        </div>
+      </div>
+      <div class="col-md-6 col-sm-6 compareImgBlk ShowFirst">
+        <div class="compareImg">
+          <a href="javascript:" class="btn darkBtn">Others </a>
+          <div class="imgborder">
+            <img src="../assets/images/comparison/c1.png" class="img-fluid" />
+          </div>
+        </div>
+        <div class="compareImg">
+          <a href="javascript:" class="btn darkBtn">Ditto Dolls </a>
+          <div class="imgborder">
+            <img src="../assets/images/comparison/c2.png" class="img-fluid" />
+          </div>
+        </div>
+      </div>
+      <div class="col-1"></div>
+    </div>
+    <div class="row miniatureBlurryface whiteBg">
+      <div class="col-1"></div>
+      <div class="col-md-6 col-sm-6 compareImgBlk">
+        <div class="compareImg">
+          <a href="javascript:" class="btn darkBtn me-5">Others </a>
+          <div class="imgborder detailingImg">
+            <img src="../assets/images/comparison/c3.png" class="img-fluid" />
+          </div>
+        </div>
+        <div class="compareImg">
+          <a href="javascript:" class="btn darkBtn">Ditto Dolls </a>
+          <div class="imgborder detailingImg">
+            <img src="../assets/images/comparison/c4.png" class="img-fluid" />
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-12">
+        <div class="paraLeft compareSection">
+          <h4>extreme detailing in 3d file</h4>
+          <span>High polycount in 3D design</span>
+          <p>
+            Every 3D file have a polycount which is a 3D pixel, even in the 3D
+            file created from the scanning process have very less polycount
+            that’s why the small detailing of accessories, hairs, wrinkles, etc.
+            are missed but we deliver the most high polycount 3d file to make it
+            as realistic as possible
+          </p>
+          <a
+            href="https://wa.me/+918700101645?text=hello I'm interested in buying your miniatures kindly guide me the order instructions…"
+            class="btn cutomizeBtn"
+            target="_blank"
+            >Customize Now <img src="../assets/images/arrow-right.svg"
+          /></a>
+        </div>
+      </div>
+      <div class="col-1"></div>
+    </div>
+    <div class="row miniatureIntro pinkBg washable">
+      <div class="col-1"></div>
+      <div class="col-md-4 col-sm-12 ShowSecond">
+        <div class="paraRight compareSection">
+          <h4>Water Washable</h4>
+          <span>Easy to clean with water or wet cloth </span>
+          <p>
+            Any statue can get dirty over the time and cleaning it can be a huge
+            task if you are getting it from other brands because you cant clean
+            it with water but we use water washable materials so that it can be
+            clean easily to deliver the exact look over the years.
+          </p>
+          <a
+            href="https://wa.me/+918700101645?text=hello I'm interested in buying your miniatures kindly guide me the order instructions…"
+            class="btn cutomizeBtn"
+            target="_blank"
+            >Customize Now <img src="../assets/images/arrow-right.svg"
+          /></a>
+        </div>
+      </div>
+      <div class="col-md-6 col-sm-6 compareImgBlk ShowFirst">
+        <div class="compareImg">
+          <a href="javascript:" class="btn darkBtn">Others </a>
+          <div class="imgborder">
+            <img src="../assets/images/comparison/c5.png" class="img-fluid" />
+          </div>
+        </div>
+        <div class="compareImg">
+          <a href="javascript:" class="btn darkBtn">Ditto Dolls </a>
+          <div class="imgborder">
+            <img src="../assets/images/comparison/c6.png" class="img-fluid" />
+          </div>
+        </div>
+      </div>
+      <div class="col-1"></div>
+    </div>
+    <div class="row miniatureBlurryface whiteBg allposes">
+      <div class="col-1"></div>
+      <div class="col-md-6 col-sm-6 compareImgBlk">
+        <div class="compareImg">
+          <a href="javascript:" class="btn darkBtn me-5">Others </a>
+          <div class="imgborder">
+            <img src="../assets/images/comparison/c7.png" class="img-fluid" />
+          </div>
+        </div>
+        <div class="compareImg ShowThird">
+          <a href="javascript:" class="btn darkBtn">Ditto Dolls </a>
+          <div class="imgborder">
+            <img src="../assets/images/comparison/c8.png" class="img-fluid" />
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-12">
+        <div class="paraLeft compareSection">
+          <h4>All Poses</h4>
+          <span>No limitation for selecting image</span>
+          <p>
+            When you think of getting a miniature the very first question arises
+            in our mind is, is that even possible to make this miniature because
+            3d file making is very difficult task to do but we have a team of
+            experts who deliver their time and patience to create a 3d file
+            irrespective of any pose and dress.
+          </p>
+          <a
+            href="https://wa.me/+918700101645?text=hello I'm interested in buying your miniatures kindly guide me the order instructions…"
+            class="btn cutomizeBtn"
+            target="_blank"
+            >Customize Now <img src="../assets/images/arrow-right.svg"
+          /></a>
+        </div>
+      </div>
+      <div class="col-1"></div>
+    </div>
+    <div class="row miniatureIntro pinkBg facialExp">
+      <div class="col-1"></div>
+      <div class="col-md-4 col-sm-12 ShowSecond">
+        <div class="paraRight compareSection">
+          <h4>facial expression</h4>
+          <span>Express feelings through miniature</span>
+          <p>
+            Facial expressions are a great way to express emotions so it should
+            look exactly the same but most of the brands overlook this details
+            and the end product doesn't deliver the emotion but don't worry we
+            don't take these small details for granted.
+          </p>
+          <a
+            href="https://wa.me/+918700101645?text=hello I'm interested in buying your miniatures kindly guide me the order instructions…"
+            class="btn cutomizeBtn"
+            target="_blank"
+            >Customize Now <img src="../assets/images/arrow-right.svg"
+          /></a>
+        </div>
+      </div>
+      <div class="col-md-6 col-sm-6 compareImgBlk ShowFirst">
+        <div class="compareImg">
+          <a href="javascript:" class="btn darkBtn">Others </a>
+          <div class="imgborder">
+            <img src="../assets/images/comparison/c9.png" class="img-fluid" />
+          </div>
+        </div>
+        <div class="compareImg">
+          <a href="javascript:" class="btn darkBtn">Ditto Dolls </a>
+          <div class="imgborder">
+            <img src="../assets/images/comparison/c10.png" class="img-fluid" />
+          </div>
+        </div>
+      </div>
+      <div class="col-1"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,7 @@
     </div>
     <Banner />
     <Row />
+    <Comparison />
     <Products />
     <ChooseTheBest />
     <HowItWorks />
