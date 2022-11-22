9b40e0a7f9a26a68b99234ea903f74dae770fdb6
Ekta Chaudhary <60789158+itsekta@users.noreply.github.com>
2022-11-22 19:45:25 +0530

    Commented Code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,10 +23,10 @@ export default defineNuxtConfig({
   vite: {
     define: { "process.env.DEBUG": false },
   },
-  publicRuntimeConfig: {
-    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-  },
-  privateRuntimeConfig: {
-    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-  },
+  // publicRuntimeConfig: {
+  //   FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+  // },
+  // privateRuntimeConfig: {
+  //   FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+  // },
 });
